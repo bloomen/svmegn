83203bedb3478eadde41d88f8046b66aefc3c6bb
Christian Blume <chr.blume@gmail.com>
2022-12-28 12:45:44 +1300

    dynamic lib macos support

--- env.sh
+++ env.sh
@@ -17,3 +17,4 @@ libsvm=$(myrealpath ~/.conan/data/libsvm/330/_/_/package/*/ | head -n 1)
 export CXXFLAGS="-I$eigen/include -I$libsvm/include"
 export LDFLAGS="-L$libsvm/lib"
 export LD_LIBRARY_PATH="$libsvm/lib"
+export DYLD_LIBRARY_PATH="$libsvm/lib"
