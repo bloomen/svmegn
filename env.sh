9b1d10aaa91d1b99957de90627461e2aef34a1e9
Christian Blume <chr.blume@gmail.com>
2022-12-28 12:39:38 +1300

    enable macos build

--- env.sh
+++ env.sh
@@ -1,5 +1,19 @@
-eigen=$(realpath ~/.conan/data/eigen/3.4.0/_/_/package/*/ | head -n 1)
-libsvm=$(realpath ~/.conan/data/libsvm/330/_/_/package/*/ | head -n 1)
+myrealpath() {
+  OURPWD=$PWD
+  cd "$(dirname "$1")"
+  LINK=$(readlink "$(basename "$1")")
+  while [ "$LINK" ]; do
+    cd "$(dirname "$LINK")"
+    LINK=$(readlink "$(basename "$1")")
+  done
+  REALPATH="$PWD/$(basename "$1")"
+  cd "$OURPWD"
+  echo "$REALPATH"
+}
+
+eigen=$(myrealpath ~/.conan/data/eigen/3.4.0/_/_/package/*/ | head -n 1)
+libsvm=$(myrealpath ~/.conan/data/libsvm/330/_/_/package/*/ | head -n 1)
+
 export CXXFLAGS="-I$eigen/include -I$libsvm/include"
 export LDFLAGS="-L$libsvm/lib"
 export LD_LIBRARY_PATH="$libsvm/lib"
