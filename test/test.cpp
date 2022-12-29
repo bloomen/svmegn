b77ec0a0dc299096d0f09f9ace8c1d436088f7d5
Christian Blume <chr.blume@gmail.com>
2022-12-29 18:00:43 +1300

    use external catch2

--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,12 @@
-#define CATCH_CONFIG_MAIN
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wconversion"
-#include "catch_amalgamated.hpp"
-#pragma GCC diagnostic pop
-#include "svmegn.h"
+#include <catch2/catch_all.hpp>
+#include <svmegn.h>
 
 TEST_CASE("something")
 {
+}
+
+int
+main(int argc, char** argv)
+{
+    return Catch::Session().run(argc, argv);
 }
