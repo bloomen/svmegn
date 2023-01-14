baca828e8e0e14f8b03eec88e293bf888f7dca80
Christian Blume <chr.blume@gmail.com>
2023-01-15 11:47:06 +1300

    improve doc

--- README.md
+++ README.md
@@ -2,13 +2,18 @@
 
 [![Actions](https://github.com/bloomen/svmegn/actions/workflows/svmegn-tests.yml/badge.svg?branch=main)](https://github.com/bloomen/svmegn/actions/workflows/svmegn-tests.yml?query=branch%3Amain)
 
-This a C++ wrapper library around [libsvm](https://www.csie.ntu.edu.tw/~cjlin/libsvm/) & [liblinear](https://www.csie.ntu.edu.tw/~cjlin/liblinear/) using the [Eigen](https://eigen.tuxfamily.org) linear algebra library.
+This is a C++ wrapper library around [libsvm](https://www.csie.ntu.edu.tw/~cjlin/libsvm/) and [liblinear](https://www.csie.ntu.edu.tw/~cjlin/liblinear/) using the [Eigen](https://eigen.tuxfamily.org) linear algebra library.
 
-Sample code:
+Sample usage:
 ```
+// Let X be the Eigen matrix of features (dense or sparse)
+// Let y be the Eigen vector of targets (class labels in this case)
 svmegn::Params params;
+params.model_type = svmegn::ModelType::SVM; // = libsvm. Use LINEAR for liblinear
+params.svm_type = svmegn::SvmType::C_SVC;
 params.C = 10;
 params.gamma = 0.1;
 const auto model = svmegn::Model::train(params, X, y);
-const auto pred = model.predict(X);
+const auto prediction = model.predict(X);
+// prediction.y is now the Eigen vector of responses
 ```
--- include/svmegn.h
+++ include/svmegn.h
@@ -30,23 +30,29 @@ using MatrixD =
     Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
 using SpaMatrixD = Eigen::SparseMatrix<double, Eigen::RowMajor>;
 
+// The model type used behind the scenes
 enum ModelType : svmegn::SmallInt
 {
-    SVM = 0, // libsvm
-    LINEAR = 1 // liblinear
+    SVM = 0, // libsvm. Linear/Non-linear regression and classification
+    LINEAR =
+        1 // liblinear. Linear regression and classification for large data sets
 };
 
+// Returns the version of the underlying library
 int
 impl_library_version(svmegn::ModelType model_type);
 
+// Sets the print function for debugging purposes
 void
 set_print_string_function(svmegn::ModelType model_type,
                           void (*func)(const char*));
 
+// Removes the print function
 void
 remove_print_string_function(svmegn::ModelType model_type);
 
-enum class SvmType : svmegn::SmallInt // Used for ModelType::SVM
+// Classification/regression algroithm used for ModelType::SVM
+enum class SvmType : svmegn::SmallInt
 {
     C_SVC = 0,
     NU_SVC = 1,
@@ -55,7 +61,8 @@ enum class SvmType : svmegn::SmallInt // Used for ModelType::SVM
     NU_SVR = 4
 };
 
-enum class KernelType : svmegn::SmallInt // Used for ModelType::SVM
+// Kernel used for ModelType::SVM
+enum class KernelType : svmegn::SmallInt
 {
     LINEAR = 0,
     POLY = 1,
@@ -64,7 +71,8 @@ enum class KernelType : svmegn::SmallInt // Used for ModelType::SVM
     PRECOMPUTED = 4
 };
 
-enum class LinearType : svmegn::SmallInt // Used for ModelType::Linear
+// Classification/regression algroithm used for ModelType::LINEAR
+enum class LinearType : svmegn::SmallInt
 {
     L2R_LR = 0,
     L2R_L2LOSS_SVC_DUAL = 1,
@@ -82,6 +90,7 @@ enum class LinearType : svmegn::SmallInt // Used for ModelType::Linear
 
 struct Params
 {
+    // The model type used behind the scenes
     svmegn::ModelType model_type = svmegn::ModelType::SVM;
 
     // For ModelType::SVM
@@ -105,7 +114,7 @@ struct Params
     // For ModelType::SVM. In MB
     double cache_size = 200;
 
-    // For ModelType::SVM/LINEAR. stopping criteria
+    // For ModelType::SVM/LINEAR. Stopping criteria
     double eps = 0.001;
 
     // For ModelType::SVM/LINEAR. For C_SVC, EPSILON_SVR and NU_SVR
@@ -150,8 +159,8 @@ public:
 
 struct Prediction
 {
-    svmegn::VectorD y;
-    std::optional<svmegn::MatrixD> prob;
+    svmegn::VectorD y; // The response values
+    std::optional<svmegn::MatrixD> prob; // Optional probability estimates
 };
 
 class Model
@@ -167,6 +176,7 @@ public:
     Model&
     operator=(Model&&);
 
+    // Trains a model given features X and target y
     static Model
     train(svmegn::Params params,
           const svmegn::MatrixD& X,
@@ -176,6 +186,9 @@ public:
           const svmegn::SpaMatrixD& X,
           const svmegn::VectorD& y);
 
+    // Runs a cross-validation given features X and target y.
+    // It splits the training data into nr_folds.
+    // The returned response is for the folds left out.
     static svmegn::VectorD
     cross_validate(const svmegn::Params& params,
                    const svmegn::MatrixD& X,
@@ -187,26 +200,34 @@ public:
                    const svmegn::VectorD& y,
                    int nr_fold = 10);
 
+    // Returns the model parameters
     const svmegn::Params&
     params() const;
 
+    // Returns the number of features aka X.cols()
     int
     nr_features() const;
 
+    // Returns the number of class labels
     int
     nr_class() const;
 
+    // Returns the class labels
     std::optional<svmegn::VectorI>
     labels() const;
 
+    // Predicts a response given features X. If prob = true
+    // then it will also compute probabilities.
     svmegn::Prediction
     predict(const svmegn::MatrixD& X, bool prob = false) const;
     svmegn::Prediction
     predict(const svmegn::SpaMatrixD& X, bool prob = false) const;
 
+    // Saves the model to the given output stream
     void
     save(std::ostream& os) const;
 
+    // Loads the model from the given input stream
     static Model
     load(std::istream& is);
 
